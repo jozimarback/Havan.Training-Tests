36db9b233e8da945693bda9daaa67ddbd3981412
Jozimar Back <jozimar.back@havan.com.br>
2020-01-13 14:36:27 -0300

    Criação de projeto e solução.

---
+++ Havan.Training-Tests.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Havan.Training_Tests.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
