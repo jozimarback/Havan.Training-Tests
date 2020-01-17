10c20846dc317e7c5b87085e57689fa3bcb40475
Jozimar Back <jozimar.back@havan.com.br>
2020-01-17 08:43:34 -0300

    Criação de projeto de teste de integração

---
+++ Havan.Training-Tests.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Havan.Training_Tests.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
