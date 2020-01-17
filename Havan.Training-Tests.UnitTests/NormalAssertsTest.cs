6c5319cda4caaf8f20f4fec8b576ac15c03401ee
Jozimar Back <jozimar.back@havan.com.br>
2020-01-17 13:09:22 -0300

    Criação de testes

--- Havan.Training-Tests.UnitTests/NormalAssertsTest.cs
+++ Havan.Training-Tests.UnitTests/NormalAssertsTest.cs
@@ -1,14 +1,41 @@
-using System;
+using Havan.CaixaEletronico.Domain;
 using Xunit;
 
 namespace Havan.Training_Tests.UnitTests
 {
-    public class UnitTest1
+    public class NormalAssertsTest
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly Caixa caixa = new Caixa();
 
+        [Fact]
+        public void SaqueContemMenorNumeroDeCedulas()
+        {
+            int quantidadeDeCedulas = 3;
+            int valorDoSaque = 80;
+
+            var resultadoCedulas = caixa.Saque(valorDoSaque);
+
+            Assert.Equal(quantidadeDeCedulas, resultadoCedulas.Count);
+        }
+
+        [Fact]
+        public void SaqueComCedulasIndisponiveis()
+        {
+            int valorDoSaque = 45;
+
+            bool saqueEhValido = caixa.ValidaCedulasDisponiveis(valorDoSaque);
+
+            Assert.False(saqueEhValido);
+        }
+
+        [Fact]
+        public void SaqueValido()
+        {
+            int valorDoSaque = 510;
+
+            bool saqueEhValido = caixa.ValidaCedulasDisponiveis(valorDoSaque);
+
+            Assert.True(saqueEhValido);
         }
     }
 }
