2d9b8327b33c65adb409d65f991910f3aac1f52d
Jozimar Back <jozimar.back@havan.com.br>
2020-01-21 11:46:24 -0300

    Criação de moq de testes

---
+++ Havan.Training-Tests.UnitTests/02 - Moq/MoqTests.cs
@@ -0,0 +1,28 @@
+﻿using Havan.CaixaEletronico.Domain;
+using Moq;
+using NSubstitute;
+using Xunit;
+
+namespace Havan.Training_Tests.UnitTests
+{
+    public class MoqTests
+    {
+        [Fact(DisplayName ="Saque sem retorno cedulas NSubstitute")]
+        public void SaqueContemNenhumaCedulasNSUb()
+        {
+            var caixa = Substitute.For<ICaixa>();
+            int valorDoSaque = 50;
+            caixa.Saque(valorDoSaque).Returns(new int[] { });
+            caixa.Received(1);
+        }
+
+        [Fact(DisplayName = "Saque sem retorno cedulas Moq")]
+        public void SaqueContemNenhumaCedulasMoq()
+        {
+            var caixa = new Mock<ICaixa>();
+            int valorDoSaque = 50;
+            caixa.Object.Saque(valorDoSaque);
+            caixa.Verify(r => r.Saque(valorDoSaque),Times.Once);
+        }
+    }
+}
