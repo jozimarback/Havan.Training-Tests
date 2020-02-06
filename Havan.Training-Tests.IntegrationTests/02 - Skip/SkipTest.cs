5c2b17f1757469da25fc77ea494568a8a1f322ab
Jozimar Back <jozimar.back@havan.com.br>
2020-02-06 09:56:42 -0300

    Modificado exemplo skip test

--- Havan.Training-Tests.IntegrationTests/02 - Skip/SkipTest.cs
+++ Havan.Training-Tests.IntegrationTests/02 - Skip/SkipTest.cs
@@ -1,13 +1,28 @@
-﻿using Xunit;
+﻿using Havan.CaixaEletronico.Api;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace Havan.Training_Tests.IntegrationTests
 {
+    [Collection(nameof(IntegrationApiTestFixtureCollection))]
     public class SkipTest
     {
-        [Fact(DisplayName = "Número de cedula confere", Skip = "Teste defasado, não mais necessário")]
-        public void NumeroCedulaConfere()
+        private readonly IntegrationTestFixture<StartupApiTests> _integrationTestFixture;
+
+        public SkipTest(IntegrationTestFixture<StartupApiTests> integrationTestFixture)
         {
-            Assert.Equal(CaixaEletronico.Domain.Cedula.Cem, 100);
+            _integrationTestFixture = integrationTestFixture;
+
+        }
+
+        [Fact(DisplayName = "Saque com sucesso", Skip = "Teste defasado, não mais necessário")]
+        public async Task Saque_com_sucesso()
+        {
+            var requisicao = await _integrationTestFixture.Client.PostAsJsonAsync($"/api/CaixaEletronico/saque/100", new { });
+            var resposta = await requisicao.Content.ReadAsStringAsync();
+
+            Assert.True(requisicao.IsSuccessStatusCode);
         }
     }
 }
